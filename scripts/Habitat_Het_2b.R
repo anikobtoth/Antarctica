a8a5596913dcf8735e7ce1d0a9d0eaa2ae143cd5
Aniko B. Toth <abtoth2@gmail.com>
2020-05-11 19:48:25 +1000

    Add scripts for analysing species and habitats

---
+++ scripts/Habitat_Het_2b.R
@@ -0,0 +1,108 @@
+## This script classifies Antarctic ice-free area into habitats using fuzzy 
+#c-means clustering.
+#The input layers are on a 1km resolution (bedmap grid) and were compiled into
+# a matrix for use here (see: "Habitat_matrix_updated_Dec18.R")
+
+library(e1071)
+library(data.table)
+library(tidyverse)
+library(ggfortify)
+
+## Load data table required for appending to output at end (i.e. lat/lon,
+# # of species, # of genera, # of records)
+req_var = fread("./data/Habitats/req_var.csv")
+
+## Load data table of environmental predictors with the IF cells only, 
+#with all na values removed and normalised on a scale of 0 to 1
+envpred_norm = fread("./data/Habitats/envpred_norm.csv")
+
+# read in habitat data (ACBR join)
+habDat <- read_csv("data/Habitats/Hab8_ACBR_join.csv")
+
+########### PCA analysis ############################
+
+x <- princomp(envpred_norm[,2:15] %>% select(-sumtemp, -rugos, -rad, -melt, -modT_0315, -DDminus5))
+
+autoplot(x, data = habDat, col = "n8U", 
+         loadings = TRUE, loadings.label = TRUE, 
+         loadings.label.size = 5, alpha = 0.4)
+
+
+library(rgl)
+plot3d(x$scores[,1:3], col=habDat$n8U)
+text3d(x$loadings[,1:3], texts=rownames(x$loadings), col="red")
+
+habDat$n8U <- cmeans(envpred_norm %>% select(-sumtemp, -rugos, -rad, -melt, -modT_0315, -DDminus5), 
+       8, iter.max = 1000,verbose = FALSE, 
+       dist = "euclidean", method = "ufcl", m = 2, 
+       rate.par = 0.3, weights = 1)$cluster
+
+########### UNWEIGHTED UNSUPERVISED FUZZY c-MEANS ########################
+
+##NOTE: All parameters included up to this point, though may want to
+#consider removing some (such as either temp or modT_01315) if they are 
+#highly correlated (otherwise they may end up with more emphasis in clustering)
+
+### 6 groups Unweighted 1000 iterations
+UFCM_n6_U = cmeans(envpred_norm %>% select(-sumtemp, -rugos, -rad, -melt, -modT_0315, -DDminus5), 
+                   6, iter.max = 1000,verbose = FALSE, 
+                   dist = "euclidean", method = "ufcl", m = 2, 
+                   rate.par = 0.3, weights = 1)
+
+UFCM_n6_U_DT =req_var
+UFCM_n6_U_DT <- cbind(UFCM_n6_U_DT, UFCM_n6_U$membership)
+UFCM_n6_U_DT$n6_U <-UFCM_n6_U$cluster
+write.csv(UFCM_n6_U_DT, file="UFCM_n6_U.csv") 
+
+### 6 groups Weigted 1000 iterations
+UFCM_n6_W = cmeans(envpred_norm, 6, iter.max = 1000,verbose = FALSE, 
+                   dist = "euclidean", method = "ufcl", m = 2, 
+                   rate.par = 0.3, weights = req_var$rck01_prop)
+
+UFCM_n6_W_DT =req_var
+UFCM_n6_W_DT <-cbind(UFCM_n6_W_DT, UFCM_n6_W$membership)
+UFCM_n6_W_DT$n6_W <-UFCM_n6_W$cluster
+write.csv(UFCM_n6_W_DT, file="UFCM_n6_W.csv")
+
+### 8 groups Unweighted 1000 iterations
+UFCM_n8_U = cmeans(envpred_norm, 8, iter.max = 1000,verbose = FALSE, 
+                   dist = "euclidean", method = "ufcl", m = 2, 
+                   rate.par = 0.3, weights = 1)
+
+UFCM_n8_U_DT =req_var
+UFCM_n8_U_DT<- cbind(UFCM_n8_U_DT, UFCM_n8_U$membership)
+UFCM_n8_U_DT$n8_U <-UFCM_n8_U$cluster
+write.csv(UFCM_n8_U_DT, file="UFCM_n8_U.csv")
+
+### 8 groups Weighted 1000 iterations
+UFCM_n8_W = cmeans(epred_norm_narm, 8, iter.max = 1000,verbose = FALSE, 
+                   dist = "euclidean", method = "ufcl", m = 2, 
+                   rate.par = 0.3, weights = req_var$rck01_prop)
+
+UFCM_n8_W_DT =req_var
+UFCM_n8_W_DT$member <-UFCM_n8_W$membership
+UFCM_n8_W_DT$n8_W <-UFCM_n8_W$cluster
+write.csv(UFCM_n8_W_DT, file="UFCM_n8_W.csv")
+
+### 10 groups Unweighted 1000 iterations
+UFCM_n10_U = cmeans(epred_norm_narm, 10, iter.max = 1000,verbose = FALSE, 
+                   dist = "euclidean", method = "ufcl", m = 2, 
+                   rate.par = 0.3, weights = 1)
+
+UFCM_n10_U_DT =req_var
+UFCM_n10_U_DT$member <-UFCM_n10_U$membership
+UFCM_n10_U_DT$n10_U <-UFCM_n10_U$cluster
+write.csv(UFCM_n10_U_DT, file="UFCM_n10_U.csv")
+
+### 10 groups Weighted 1000 iterations
+UFCM_n10_W = cmeans(epred_norm_narm, 10, iter.max = 1000,verbose = FALSE, 
+                   dist = "euclidean", method = "ufcl", m = 2, 
+                   rate.par = 0.3, weights = req_var$rck01_prop)
+
+UFCM_n10_W_DT =req_var
+UFCM_n10_W_DT$member <-UFCM_n10_W$membership
+UFCM_n10_W_DT$n10_W <-UFCM_n10_W$cluster
+write.csv(UFCM_n10_W_DT, file="UFCM_n10_W.csv")
+
+#NOTE: still hasent converged before 1000 iterations, so may wish to
+#consider using more
---
+++ scripts/Helper_Functions.R
@@ -0,0 +1,150 @@
+
+library(sp)
+library(rlang)
+
+##### DATA MANIPULATION #####
+### Change character vectors in df to factors
+tofac <- function(df){
+  df[,map_lgl(df, is.character)] <- map(df[,map_lgl(df, is.character)], factor)
+  return(df)
+}
+
+### Abundance to presence-absence 
+# matrix list
+tobinary <- function(PA.LIST){
+  binary <- lapply(PA.LIST, function(x) {
+    x <- x/x
+    x[is.na(x)] <- 0
+    return(x)
+  })
+  return(binary)
+}
+
+# single matrix
+tobinary.single <- function(x) {
+  x <- x/x
+  x[is.na(x)] <- 0
+  return(x)}
+
+# First column to rownames
+namerows <- function(table){
+  rownames(table) <- table[,1]
+  table <- table[,2:ncol(table)]
+  return(table)
+}
+
+# remove empty rows and columns in 1 matrix
+# or remove rows and columns with too few observations
+clean.empty <- function(x, mincol = 1, minrow = 1){
+  x <- x[which(rowSums(x) > minrow-1),]
+  x <- x[,which(colSums(x) > mincol-1)]
+  return(x)
+}
+
+# triangular distance matrix to long format
+dist2edgelist <- function(z, sppDat){  #edge list with link types attached
+  k3 = as.matrix(z)
+  dimnames(k3) <- list(rownames(sppDat), rownames(sppDat)) 
+  
+  xy <- t(combn(colnames(k3), 2))
+  k3 <- data.frame(xy, dist=k3[xy], stringsAsFactors = F)
+  
+  k3 <- data.frame(k3, id = paste(k3$X1, k3$X2, sep = "-"), stringsAsFactors = F)
+  colnames(k3) <- c("Sp1", "Sp2", "Z.Score", "id")
+  
+  return(k3)
+}
+
+
+##### ANALYSES ######
+# FETmP
+simpairs <- function(x){ #simpairs function, simpairs only out
+  samples = ncol(x)  #S
+  z = matrix(nrow=nrow(x),ncol=nrow(x),data=0)
+  occs = array()
+  
+  #convert to P/A. Occs = rowsums of PA matrix.
+  x <- x/x
+  x[is.na(x)] <- 0
+  occs <- rowSums(x)
+  
+  #SimPairs Algorithm
+  for (i in 2:nrow(x))  {
+    for (j in 1:(i-1))
+    {
+      a = length(which(x[i,] > 0 & x[j,] > 0)) # B
+      
+      #simpairs
+      for (k in 0:a)
+        z[i,j] = z[i,j] + choose(occs[j] , k) * choose(samples - occs[j] , occs[i] - k) / choose(samples , occs[i])
+      z[i,j] = z[i,j] - choose(occs[j] , a) * choose(samples - occs[j] , occs[i] - a) / choose(samples , occs[i]) / 2
+      z[i,j] = qnorm(z[i,j])
+      z[j,i] = z[i,j]
+    }
+  }
+  print("check")
+  return(as.dist(z, diag = F, upper = F))
+}
+
+### Forbes index functions by J. Alroy ###
+##### Available from http://bio.mq.edu.au/~jalroy/Forbes.R ###
+##### help page http://bio.mq.edu.au/~jalroy/Forbes.html ###
+
+forbes<-function(x,y,corrected)	{
+  if (missing(corrected))	{
+    corrected <- T
+  }
+  if (is.numeric(x) && is.numeric(y) && min(x) == 0 && min(y) == 0 && length(x) == length(y))	{
+    a <- length(which((x * y) > 0))
+    b <- length(which(x > 0)) - a
+    c <- length(which(y > 0)) - a
+  } else	{
+    a <- length(na.omit(match(x,y)))
+    b <- length(x) - a
+    c <- length(y) - a
+  }
+  n <- a + b + c
+  if (corrected == T)	{
+    return(a * (n + sqrt(n))/((a + b) * (a + c) + a * sqrt(n) + (b * c)/2))
+  } else	{
+    return(a * n/((a + b) * (a + c)))
+  }
+}
+
+forbesMatrix<-function(x,corrected)	{
+  x[is.na(x)] <- 0
+  m = matrix(nrow=ncol(x),ncol=ncol(x))
+  for (i in 1:ncol(x))	{
+    for (j in 1:ncol(x))	{
+      m[i,j] = forbes(x[,i],x[,j],corrected)
+    }
+  }
+  m <- as.data.frame(m)
+  rownames(m) <- colnames(x)
+  colnames(m) <- colnames(x)
+  return(m)
+}
+
+
+#### Extract sites of species #
+
+getSites2 <- function(species, PA){
+  cS <- PA %>% filter(rownames(.) %in% species)
+  cS <- cS %>% select(which(colSums(cS) > 0))
+  cS <- colSums(cS)#/nrow(cS)
+  return(cS)
+}
+
+getSites <- function(clusters, PA){
+  out <- list()
+  for(i in seq_along(clusters)){
+    out[[i]] <- getSites2(clusters[[i]], PA)
+  }
+  return(out)
+}
+
+
+## special merge ##
+multimerge <- function(x, y){
+  merge(x, y, by = 0, all = TRUE) %>% namerows()
+}
