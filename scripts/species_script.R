eb6a03b6e12d8a3c3a3191a4464fb2648ad5716f
Aniko B. Toth <abtoth2@gmail.com>
2020-05-14 13:14:15 +1000

    Write code to analyse cluster groupings, characteristic assemblages, and assign IFAs to assemblage types.

--- scripts/species_script.R
+++ scripts/species_script.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(igraph)
+library(reshape2)
 source('C:/Users/Aniko/Desktop/Antarctica/scripts/Helper_Functions.R')
 
 # Load data ####
@@ -12,6 +13,10 @@ occurrences <- occurrences %>% select(scientificName, vernacularName,
                                       family, genus, species, Snap_IFA, 
                                       Dist_IFA, coordinateUncertaintyInMetres, 
                                       individualCount)
+
+## Species data ####
+sppDat <- occurrences %>% select(scientificName, vernacularName, kingdom, phylum, class, order, family, genus, species) %>% unique()
+
 # Occurrence in ice-free areas (Ice-free patches as sites).
 occ <- read_csv("data/Species/Spp_iceFree_occ.csv")
 
@@ -26,26 +31,80 @@ Abund <- PA0 %>% split(.$year %/% 10) %>%
 PA <- tobinary(Abund) %>% lapply(clean.empty, mincol = 2, minrow = 2)
 
 ## pairs analysis on assemblages ####
-test <- lapply(PA, simpairs)
-el <- map2(test, PA, dist2edgelist) #%>% bind_rows(.id = "decade")
+pairs <- lapply(PA, simpairs)
+el <- map2(pairs, PA, dist2edgelist) #%>% bind_rows(.id = "decade")
 
-g <- el %>% purrr::map(filter, Z.Score > quantile(Z.Score, 0.975, na.rm = T)) %>% 
+g <- el %>% purrr::map(filter, Z.Score > quantile(Z.Score, 0.98, na.rm = T)) %>% 
   #purrr::map(mutate, weight = Z.Score) %>%
   purrr::map(graph_from_data_frame, directed = F)
 
 mod <- purrr::map(g, cluster_fast_greedy)
 
-s <- map2(mod, PA, getSites)
+## Assemblage clusters similarity analysis #####
 
-assemb <- lapply(s, lapply, data.frame) %>% purrr::map(~reduce(., multimerge))
-assemb <- purrr::map(assemb, function(x) x %>% setNames(paste("X", 1:ncol(x), sep = "")))
+clust <- purrr::map(mod, ~data.frame(names = .$names, cluster = .$membership)) %>% 
+  bind_rows(.id = "decade") %>% mutate(clustID = paste(decade, cluster, sep = "_"))
+clustm <- dcast(clust, names~clustID, fun.aggregate = length, fill = 0) %>% namerows()
+
+# ordination
+cdist <- forbesMatrix(clustm) %>% as.dist(upper = F)
+cord <- cmdscale(1-cdist, k = 2) %>% data.frame(cluster = colnames(clustm), .)
+cord <- cord %>% mutate(decade = word(cluster, 1, 1, sep="_"), cID = word(cluster, 2, 2, sep="_"))
+
+ggplot(cord, aes(x = X1, y = X2, col = decade, label = cID)) + geom_point() + geom_text(hjust = 0, vjust = 0)
+
+#pairs
+cpairs <- clustm %>% t() %>% simpairs()
+cel <- dist2edgelist(cpairs, t(clustm))
+
+cg <- cel %>% dplyr::filter(Z.Score > quantile(Z.Score, 0.98, na.rm = T)) %>% graph_from_data_frame(directed = F)
+V(cg)$decade <- V(cg)$name %>% word(1,1, sep = "_")
+plot(cg, vertex.label = NA, vertex.size = 4, vertex.color = cmod$membership)
+cmod <- cluster_fast_greedy(cg)
+cgroups <- data.frame(cluster = cmod$names, group = cmod$membership)
+cgroups <- merge(cord, cgroups) %>% mutate(cID = as.numeric(cID))
+## Characterise sites by assemblage ####
+
+s_count <- map2(mod, PA, getSites, type = "count")
+s_prcnt <- map2(mod, PA, getSites, type = "percent")
+
+assemb1 <- lapply(s_count, lapply, data.frame) %>% purrr::map(~reduce(., multimerge))
+assemb1 <- purrr::map(assemb1, function(x) x %>% setNames(paste("X", 1:ncol(x), sep = "")))
+assemb1 <- assemb1 %>% purrr::map(~apply(.,1, function(x) which(x == max(x, na.rm = TRUE)) %>% 
+                                data.frame()) %>% 
+                                  bind_rows(.id = "IFA")) %>% bind_rows(.id = "decade")
+asmb1 <- assemb1 %>% group_by(decade, IFA) %>% summarise(count = paste0(., collapse = ";"))
+
+assemb2 <- lapply(s_prcnt, lapply, data.frame) %>% purrr::map(~reduce(., multimerge))
+assemb2 <- purrr::map(assemb2, function(x) x %>% setNames(paste("X", 1:ncol(x), sep = "")))
+assemb2 <- assemb2 %>% purrr::map(~apply(.,1, function(x) which(x == max(x, na.rm = TRUE)) %>% 
+                                           data.frame()) %>% 
+                                    bind_rows(.id = "IFA")) %>% bind_rows(.id = "decade")
+asmb2 <- assemb2 %>% group_by(decade, IFA) %>% summarise(percent = paste0(., collapse = ";"))
+assemblages <- merge(asmb1, asmb2, all = TRUE)
+
 
 ## PCoA on assemblages ####
 dist <- map(PA, forbesMatrix) %>% map(as.dist, upper = F) %>% map(~return(1-.)) 
-ord <- map(dist, cmdscale, k = 2) %>% map(data.frame)
+ord <- map(dist, cmdscale, k = 2) %>% map2(PA, function(x, y) data.frame(IFA = colnames(y), x))
 ord <- bind_rows(ord, .id = "decade")
-#ord <- map(ord, ~merge(., y = sitedat[,c(8:16)], all.x = T, all.y = F, by.x = 0, by.y = "siteid"))
 
-#####
+IFA <- merge(ord, assemblages, all = TRUE) %>% mutate(IFA = as.numeric(IFA))
+
+
+cID <- map2(IFA$decade, IFA$count %>% strsplit(";"), paste, sep = "_")
+IFA$grp.count <- lapply(cID, function(x) cgroups$group[which(cgroups$cluster %in% x)]) %>% 
+  purrr::map(unique) %>% sapply(paste0, collapse = ";")
+cID <- map2(IFA$decade, IFA$percent %>% strsplit(";"), paste, sep = "_")
+IFA$grp.percent <- lapply(cID, function(x) cgroups$group[which(cgroups$cluster %in% x)]) %>% 
+  purrr::map(unique) %>% sapply(paste0, collapse = ";")
+
+IFA <- merge(IFA, occ %>% select(OBJECTID, ACBR_Name) %>% unique(), by.x = "IFA", by.y = "OBJECTID", all.x = TRUE)
+## Species allocation in cluster groups #####
+species <- melt(clustm %>% mutate(species = rownames(clustm))) %>% 
+  filter(value > 0) %>% 
+  merge(cgroups %>% select(cluster, group), by.x = "variable", by.y = "cluster", all = TRUE)
+
+sppDat <- merge(sppDat, species, by.x = "scientificName", by.y = "species", all = TRUE)
 #####
 #####
