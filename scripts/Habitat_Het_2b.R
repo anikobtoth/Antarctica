9bea7be958995c2f8af6b8c1442f2b98ff0a04f5
Aniko B. Toth <abtoth2@gmail.com>
2020-06-16 14:40:30 +1000

    minor tweaks

--- scripts/Habitat_Het_2b.R
+++ scripts/Habitat_Het_2b.R
@@ -24,27 +24,27 @@ habDat <- read_csv("data/Habitats/Hab8_ACBR_join.csv")
 vars1 <- c("elev", "rugos", "precip", "temp", "coast", "wind")
 vars2 <- c("V1", "coast", "geoT", "cloud", "wind", "temp", "elev", "slope",  "precip")
 vars <- c("elev", "rugos", "precip", "DDminus5", "coast", "wind", "rad")
-habDat$n8U2 <- cmeans(envpred_norm %>% select(vars2), 
+habDat$n8U1 <- cmeans(envpred_norm %>% select(vars1), 
                      8, iter.max = 1000,verbose = FALSE, 
                      dist = "euclidean", method = "ufcl", m = 2, 
                      rate.par = 0.3, weights = 1)$cluster %>% as.factor()
 
-habDat$n6U2 <- cmeans(envpred_norm %>% select(vars2), 
+habDat$n6U1 <- cmeans(envpred_norm %>% select(vars1), 
                      6, iter.max = 1000,verbose = FALSE, 
                      dist = "euclidean", method = "ufcl", m = 2, 
                      rate.par = 0.3, weights = 1)$cluster %>% as.factor()
 
-habDat$n8W1 <- cmeans(envpred_norm %>% select(vars1), 
+habDat$n8W2 <- cmeans(envpred_norm %>% select(vars2), 
                      8, iter.max = 1000,verbose = FALSE, 
                      dist = "euclidean", method = "ufcl", m = 2, 
                      rate.par = 0.3, weights = req_var$rck01_prop)$cluster %>% as.factor()
 
-habDat$n6W1 <- cmeans(envpred_norm %>% select(vars1), 
+habDat$n6W2 <- cmeans(envpred_norm %>% select(vars2), 
                      6, iter.max = 1000,verbose = FALSE, 
                      dist = "euclidean", method = "ufcl", m = 2, 
                      rate.par = 0.3, weights = req_var$rck01_prop)$cluster %>% as.factor()
 
-habDat$n8W3 <- cmeans(envpred_norm %>% select(vars), 
+habDat$n8W3.2 <- cmeans(envpred_norm %>% select(vars), 
                       8, iter.max = 1000,verbose = FALSE, 
                       dist = "euclidean", method = "ufcl", m = 2, 
                       rate.par = 0.3, weights = req_var$rck01_prop)$cluster %>% as.factor()
@@ -57,7 +57,7 @@ lapply(names(habDat)[11:19], function(j) autoplot(x, data = habDat, col = j,
          loadings = TRUE, loadings.label = TRUE, 
          loadings.label.size = 5, alpha = 0.4))
 
-autoplot(x, data = habDat, col = "n8W3", 
+autoplot(x, data = habDat, col = "n8W3.2", 
          loadings = TRUE, loadings.label = TRUE, 
          loadings.label.size = 5, alpha = 0.4)
 
