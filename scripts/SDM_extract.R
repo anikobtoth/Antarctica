56142d24a646b8f963d76bc8ba4cd0f5982b7c3b
Aniko B Toth <abtoth2@gmail.com>
2020-06-23 18:38:24 +1000

    define projection for SDMs

--- scripts/SDM_extract.R
+++ scripts/SDM_extract.R
@@ -1,16 +1,17 @@
 library(raster)
 library(tidyverse)
-library(rts)
+
 
 l <- list.dirs("E:/Antarctica/Data/Species/final_results", recursive = FALSE) %>% 
   file.path("cif_basedist0.tif")
 n <- list.dirs("E:/Antarctica/Data/Species/final_results", recursive = FALSE, full.names = FALSE)
 
 SDMs <- stack(l)
+crs(SDMs) <- "+proj=stere +lat_0=-90 +lat_ts=-71 +lon_0=0 +k=1 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs"
 v <- getValues(SDMs) %>% data.frame() %>% na.omit()
-
+names(v) <- n
 # Calculate thresholds at top 20% of output suitability range.
-thresh <- sapply(1:34, function(i) scale(v[,i]) %>% range(na.rm = TRUE) %>% quantile(0.80))
+thresh <- sapply(1:34, function(i) scale(v[,i]) %>% range(na.rm = TRUE) %>% quantile(0.50))
 
 # number of suitable pixels by group
 sapply(1:34, function(i) length(which(scale(v[,i]) >= thresh[i]))) %>% setNames(n)
@@ -22,5 +23,7 @@ pix <- xyFromCell(SDMs, rownames(v) %>% as.integer())
 PA <- lapply(1:34, function(i) scale(v[,i]) >= thresh[i]) %>% reduce(data.frame) %>%
   apply(2, as.numeric) %>% t() %>% data.frame()
 
-pairs <- simpairs(PA)
+pairs <- simpairs_lgnum(PA)
 el <- dist2edgelist(pairs, PA)
+
+eln <- el %>% filter(Z.Score < 0.5)
