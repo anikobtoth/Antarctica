b97e05aee663aedaaf271e18abca412a6f90944e
Aniko B Toth <abtoth2@gmail.com>
2021-05-20 17:28:25 +1000

    Create regional unit classification based on ACBRs and lumping scheme.

--- documents/Ecosystem_descriptions_dataprep.R
+++ documents/Ecosystem_descriptions_dataprep.R
@@ -230,3 +230,107 @@ for(i in seq_along(RUs)){
   v2[[i]] <- apply(runit %>% dplyr::select(x, y), 1, function(z)  others %>% filter(x %in% c(z["x"], z["x"]+1000, z["x"]-1000), y %in% c(z["y"], z["y"]+1000, z["y"]-1000))) %>%
     bind_rows()
 }
+
+
+v3 <- v2 %>% purrr::map(~table(.$RU_raw) %>% cbind) %>% setNames(RUs)
+x <- merge(table(acbrs$RU_raw) %>% cbind(), v2 %>% purrr::map(~table(.$RU_raw) %>% cbind) %>% setNames(RUs) %>% purrr::map_int(nrow) %>% cbind(), by = 0) %>% setNames(c("Runit", "Area", "Potential_merges"))
+
+x$merger <- NA
+x$adjpix <- NA
+x$merger[which(x$Potential_merges == 0 & x$Area < 100)] <- "Discard"
+x$merger[which(x$Potential_merges == 0 & x$Area >= 100)] <- "None"
+
+for(i in seq_along(v3)){
+  if(length(v3[[i]]) == 1){
+    x$merger[which(x$Runit == names(v3[i]))] <- rownames(v3[[i]])
+    x$adjpix[which(x$Runit == names(v3[i]))] <- v3[[i]][1]
+  }else if(length(v3[[i]]) > 1){
+    # check which matches are in the same unit
+    region <- word(names(v3[i]), 1, 1, sep = "_")
+    unit <- word(names(v3[i]), 2, 2, sep = "_")
+    
+    unitmatch <- grep(pattern = paste0("_", unit, "$"), rownames(v3[[i]]))
+    regionmatch <- grep(pattern = region, rownames(v3[[i]]))
+    
+    best_unitmatch <- which.max(v3[[i]][unitmatch,])
+    best_regionmatch <- which.max(v3[[i]][regionmatch,])
+    
+    if(length(unitmatch)==0){
+      x$merger[which(x$Runit == names(v3[i]))] <-  rownames(v3[[i]])[regionmatch[best_regionmatch]]
+      x$adjpix[which(x$Runit == names(v3[i]))] <- v3[[i]][regionmatch[best_regionmatch]]
+    }else{
+      if(length(regionmatch) == 0){
+        x$merger[which(x$Runit == names(v3[i]))] <- rownames(v3[[i]])[unitmatch[best_unitmatch]]
+        x$adjpix[which(x$Runit == names(v3[i]))] <- v3[[i]][unitmatch[best_unitmatch]]
+      }else{
+        if(v3[[i]][regionmatch[best_regionmatch]] > 2* v3[[i]][unitmatch[best_unitmatch]]) {
+          x$merger[which(x$Runit == names(v3[i]))] <-  rownames(v3[[i]])[regionmatch[best_regionmatch]]
+          x$adjpix[which(x$Runit == names(v3[i]))] <- v3[[i]][regionmatch[best_regionmatch]]
+        }else{
+          x$merger[which(x$Runit == names(v3[i]))] <- rownames(v3[[i]])[unitmatch[best_unitmatch]]
+          x$adjpix[which(x$Runit == names(v3[i]))] <- v3[[i]][unitmatch[best_unitmatch]]
+        }
+      }
+    }
+  }
+}
+
+
+library(igraph)
+x2 <- x %>% dplyr::select(Runit, merger, Area, weight = adjpix) %>% 
+  filter(!merger %in% c("Discard"))
+
+g <- graph_from_data_frame(x2, directed = TRUE)
+g <- delete.vertices(g, "None")
+
+plot(g, vertex.size = log(x2$Area), vertex.label.size = .5, vertex.label = NA)
+
+cl <- cluster_edge_betweenness(g)
+
+m <- numeric()
+for(i in seq_along(cl)){m[i] <- length(cl[[i]])}
+
+
+# i <- 14
+# acbrs %>% filter(RU_raw %in% cl[[i]]) %>% ggplot(aes(x = x, y = y, col = RU_raw, fill = RU_raw)) + 
+#   geom_tile(lwd = 1) + coord_equal()
+# 
+# 
+# for(i in seq_along(cl)){
+#  ggplot() + 
+#     geom_tile(acbrs %>% filter(RU_raw %in% cl[[i]]), aes(x = x, y = y, col = RU_raw, fill = RU_raw), lwd = 1) + 
+#     coord_equal() 
+# }
+# 
+
+
+
+ext <- list()
+bg <- list()
+bg_tile <- list()
+ru_merge <- list()
+p <- list()
+
+for(i in seq_along(cl)){
+  
+  ext[[i]] <- geo_bounds(acbrs %>% filter(RU_raw %in% cl[[i]]) %>% dplyr::select(y, x), buff = 0.15)
+  bg[[i]] <- crop(antarctica, extent(ext[[i]]))
+  bg_tile[[i]] <- df_crop(typ_df, ext[[i]])
+  ru_merge[[i]] <- acbrs %>% filter(RU_raw %in% cl[[i]])
+  
+  p[[i]] <- ggplot() + 
+    geom_polygon(data = bg[[i]], aes(x=long, y=lat, group=group), fill="gray50", color= NA, size=0.25) +
+    geom_tile(data = bg_tile[[i]], aes(x = x, y = y), fill = "gray35", col = "gray35", lwd = 0.8) +
+    geom_tile(data = ru_merge[[i]], aes(x = x, y = y, col = RU_raw, fill = RU_raw), lwd = 1.5) + 
+    coord_equal() +
+    theme(axis.title = element_blank(),
+          plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm")) +
+    scale_y_continuous(labels = scales::scientific) + scale_x_continuous(labels = scales::scientific) 
+  
+  save(ext, bg, bg_tile, ru_merge, p, file = "./results/RegionalUnitsPlots.RData")
+}
+
+
+rmarkdown::render('./documents/Regional_units.Rmd',  
+                  output_file =  "Regional_units.html", 
+                  output_dir = './documents')
